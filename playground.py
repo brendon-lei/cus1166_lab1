1a96286dcf74789863def0a8ff15efdea66e5135
brendon-lei <brendonlei@gmail.com>
2019-01-30 16:56:48 -0500

    second commit

--- playground.py
+++ playground.py
@@ -1,4 +1,4 @@
-print("Hello Word") # Display a message
+print("Hello Word")  # Display a message
 # Get user input and display a message.
 myname = input("What is your name: ")
 print("Hello " + str(myname))
@@ -7,6 +7,7 @@ print("Hello %s" % myname)
 
 i = 120
 print(f"Variable i has the value {i}")
+
 f = 1.6180339
 print(f"Variable f has the value {f} and its type is {type(f)}")
 b = True
@@ -14,11 +15,11 @@ print(f"Variable b has the value {b}")
 n = None
 print(f"Variable n has the value of {n}")
 # tuple
-c = (10,20,10)
+c = (10, 20, 10)
 print(f" c[0] has the value {c[0]} and is of type: {type(c)}")
 # list
 l = ["Anna", "Tom", "John"]
-l = [10,20,30]
+l = [10, 20, 30]
 print(f" l[0] has the value {l[0]} and is of type: {type(l)}")
 # Sets variables.
 s = set()
@@ -33,16 +34,22 @@ grades["Anna"] = "F"
 # Create an empty dictionary .
 mydictionary = dict()
 
-x=10
+x = 10
 if (x > 0):
     print("The number x is positive")
-elif (x<0):
+elif (x < 0):
     print("The number x is negative")
 else:
     print("The number x is zero")
 
-
 for i in range(5):
     print(i)
-for i_idx, i_value in enumerate(range(2,10)):
-    print(f"{i_idx} - {i_value}" )
+for i_idx, i_value in enumerate(range(2, 10)):
+    print(f"{i_idx} - {i_value}")
+
+
+def is_even(x):
+    if (x % 2) == 0:
+        return True
+    else:
+        return False
